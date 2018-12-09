8ce88387b62447950d078e3d01206a2866db207f
William Ho <awho168@gmail.com>
2018-12-09 15:44:47 -0600

    convert classes not in top 20 to other

--- utilities.py
+++ utilities.py
@@ -1,4 +1,4 @@
-import csv, datetime
+import csv, datetime, operator
 
 def convertTimestamps(infile, outfile):
   with open(infile) as csv_file:
@@ -19,4 +19,44 @@ def convertTimestamps(infile, outfile):
         duplicate[2] = round(time, 2)
         writer.writerow(duplicate)
 
-convertTimestamps('./data/ExplicitClasses.csv', './data/DataV1.csv')
+def convertToOtherClasses(infile, outfile, numOther):
+
+  counter = {}
+  with open(infile) as csv_file:
+    reader = csv.reader(csv_file)
+
+    skip = True
+    for row in reader:
+      if skip:
+        skip = False
+        continue
+
+      if row[20] not in counter.keys():
+        counter[row[20]] = 1
+      else:
+        counter[row[20]] += 1
+
+    sorted_counter = sorted(counter.items(), key=operator.itemgetter(1), reverse=True)
+    keep = sorted_counter[0:numOther]
+    keep_names = [tup[0] for tup in keep]
+
+  with open(infile) as csv_file:
+    with open(outfile, mode='w') as modified_file:
+      reader = csv.reader(csv_file)
+      writer = csv.writer(modified_file, delimiter=',')
+
+      inner_skip = True
+      for row in reader:
+        if inner_skip:
+          inner_skip = False
+          writer.writerow(row)
+          continue
+
+        duplicate = row.copy()
+        if duplicate[20] not in keep_names:
+          duplicate[20] = 'Other'
+
+        writer.writerow(duplicate)
+
+#convertTimestamps('./data/ExplicitClasses.csv', './data/DataV1.csv')
+convertToOtherClasses('./data/DataV1.csv', './data/DataV2.csv', 20)
